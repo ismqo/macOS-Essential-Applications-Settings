672015376c2a8ca65698f17879266e3f3062fb27
ismqo <57599812+ismqo@users.noreply.github.com>
2026-09-16 13:01:21 +0100

    Add files via upload

---
+++ brewfile.rb
@@ -0,0 +1,36 @@
+# Brewfile
+# Install with:
+# brew bundle --file="$HOME/Brewfile"
+
+# CLI applications
+brew "git"
+brew "lazygit"
+brew "mas"
+brew "node@20"
+brew "opencode"
+brew "pnpm"
+brew "starship"
+brew "yt-dlp"
+
+# GUI applications
+cask "anydesk"
+cask "google-chrome"
+cask "eqmac"
+cask "ghostty"
+cask "helium-browser"
+cask "localsend"
+cask "minecraft"
+cask "obsidian"
+cask "openvpn-connect"
+cask "protonvpn"
+cask "transmission"
+cask "visual-studio-code"
+cask "vlc"
+cask "vorssaint"
+cask "cask-hub"
+
+# Fonts
+cask "font-hack-nerd-font"
+
+# Mac App Store applications
+mas "Bitwarden", id: 1352778147
